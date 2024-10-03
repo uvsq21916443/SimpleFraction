54d856841ee4341d42fe1cb10eba72e07e1eed07
Ewan TREBERN <ewan.trebern@ens.uvsq.fr>
2024-10-03 03:28:49 +0200

    classe Fraction operationnelle

--- Fraction.java
+++ Fraction.java
@@ -1,5 +1,71 @@
-public class Fraction {
+public class Fraction extends Number implements Comparable<Fraction> {
+    private int num;
+    private int den;
+
+    public static Fraction ZERO = new Fraction(0,1);
+    public static Fraction UN = new Fraction(1,1);
+
+    public Fraction(int a, int b) {
+        num = a;
+        den = b;
+    }
+
+    public Fraction(int a) {
+        num = a;
+        den = 1;
+    }
+
+    public Fraction() {
+        num = 0;
+        den = 1;
+    }
+
     public String toString() {
-        return "Je suis une fraction.";
+        return "Je suis une fraction : " + num + '/' + den;
+    }
+
+    public int getNum() {
+        return this.num;
+    }
+
+    public int getDen() {
+        return this.den;
+    }
+
+    @Override
+    public double doubleValue() {
+        return Double.valueOf(this.num) / Double.valueOf(this.den);
+    }
+
+    public double add(Fraction f) {
+        return this.doubleValue() + f.doubleValue();
+    }
+
+    @Override
+    public int compareTo(Fraction f) {
+        if (f == null) {
+            throw new NullPointerException("paramètre null");
+        }
+
+        if (f.getClass() != Fraction.class) {
+            throw new ClassCastException("mauvais type d'objet");
+        }
+        
+        return Double.valueOf(this.doubleValue()).compareTo(Double.valueOf(f.doubleValue()));
+    }
+
+    @Override
+    public int intValue() {
+        return Double.valueOf(this.doubleValue()).intValue();
+    }
+
+    @Override
+    public long longValue() {
+        return Double.valueOf(this.doubleValue()).longValue();
+    }
+
+    @Override
+    public float floatValue() {
+        return Double.valueOf(this.doubleValue()).floatValue();
     }
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,34 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("\nJe compile");
-        System.out.println(new Fraction().toString());
+        
+        Fraction a = new Fraction(2,4);
+        Fraction b = new Fraction(2);
+        Fraction c = new Fraction();
+        Fraction d = new Fraction(1);
+
+        assert a.toString().equals("Je suis une fraction : 2/4");
+        assert b.toString().equals("Je suis une fraction : 2/1");
+        assert c.toString().equals("Je suis une fraction : 0/1");
+
+        assert Fraction.ZERO.toString().equals(c.toString());
+        assert Fraction.UN.toString().equals(d.toString());
+
+        assert a.getNum() == 2;
+        assert a.getDen() == 4;
+
+        assert a.doubleValue() == 0.5d;
+
+        assert a.add(b) == 2.5d;
+
+        assert c.doubleValue() == Fraction.ZERO.doubleValue();
+
+        assert a.compareTo(b) == -1;
+        assert a.compareTo(c) == 1;
+        assert a.compareTo(a) == 0;
+
+        Number aNumber = java.math.BigDecimal.ONE;
+        Number anotherNumber = new Fraction(1, 2);
+        assert java.lang.Math.abs(aNumber.doubleValue() + anotherNumber.doubleValue() - 1.5) < 1E-8;
     }
 }
